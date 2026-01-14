8e89901ded84279e748aa379755135d665db3937
Lee jong sik <102013392+js990311@users.noreply.github.com>
2026-01-14 21:25:59 +0900

    init:

---
+++ src/main/java/com/rejs/flashnote/FlashnoteApplication.java
@@ -0,0 +1,13 @@
+package com.rejs.flashnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rejs/flashnote/FlashnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rejs.flashnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
