d4be8731b8b968fa75f632ada59d500f84171ca6
Lee jong sik <102013392+js990311@users.noreply.github.com>
2026-01-23 22:12:07 +0900

    chore: open-spaced-repeition 사용 및 MIT 라이센스 적용

---
+++ src/main/java/com/rejs/flashnote/global/controller/StaticController.java
@@ -0,0 +1,12 @@
+package com.rejs.flashnote.global.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StaticController {
+    @RequestMapping("/license")
+    public String license(){
+        return "license";
+    }
+}
