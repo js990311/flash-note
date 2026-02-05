f5899b2ba20bcc30ec8e5b4c0284c096d4df3f60
Lee jong sik <102013392+js990311@users.noreply.github.com>
2026-02-05 22:00:52 +0900

    chore: grafana alloy를 위한 설정 사용

--- src/main/java/com/rejs/flashnote/FlashnoteApplication.java
+++ src/main/java/com/rejs/flashnote/FlashnoteApplication.java
@@ -1,13 +1,21 @@
 package com.rejs.flashnote;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlashnoteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlashnoteApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", "flashnote");
 	}
 
 }
