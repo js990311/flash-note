74a082c356f8deaee895f5391e4bf0d2d70e9db1
Lee jong sik <102013392+js990311@users.noreply.github.com>
2026-02-08 20:24:06 +0900

    feat: noteSearchService, noteSearchRepository

---
+++ src/main/java/com/rejs/flashnote/domain/note/service/NoteSearchService.java
@@ -0,0 +1,26 @@
+package com.rejs.flashnote.domain.note.service;
+
+import com.rejs.flashnote.domain.note.authorization.PreNoteReadAuthorize;
+import com.rejs.flashnote.domain.note.dto.NoteDto;
+import com.rejs.flashnote.domain.note.dto.request.note.NoteSearchOption;
+import com.rejs.flashnote.domain.note.entity.Note;
+import com.rejs.flashnote.domain.note.error.NoteException;
+import com.rejs.flashnote.domain.note.repository.NoteSearchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class NoteSearchService {
+    private final NoteSearchRepository noteSearchRepository;
+
+    @Transactional(readOnly = true)
+    @PreNoteReadAuthorize
+    public Page<NoteDto> readById(Long memberId, String keyword, NoteSearchOption searchOption, Pageable pageable){
+        return noteSearchRepository.searchNote(memberId, keyword, searchOption, pageable);
+    }
+
+}
