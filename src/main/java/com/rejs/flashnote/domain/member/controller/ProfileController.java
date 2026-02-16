245f5c8d715009511ee9adfb61471dd573fde05c
Lee jong sik <102013392+js990311@users.noreply.github.com>
2026-02-16 21:17:02 +0900

    feat: profile controller

---
+++ src/main/java/com/rejs/flashnote/domain/member/controller/ProfileController.java
@@ -0,0 +1,65 @@
+package com.rejs.flashnote.domain.member.controller;
+
+import com.rejs.flashnote.domain.member.dto.ProfileDto;
+import com.rejs.flashnote.domain.member.dto.request.UpdateProfileRequest;
+import com.rejs.flashnote.domain.member.service.MemberService;
+import com.rejs.flashnote.global.security.utils.PrincipalUtils;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/profile")
+@Controller
+public class ProfileController {
+    private final MemberService memberService;
+
+    @GetMapping("/me")
+    public String getMyProfile(Model model){
+        Long memberId = PrincipalUtils.getMemberId();
+        ProfileDto profileDto = memberService.readProfile(memberId);
+        model.addAttribute("myProfile", true);
+        model.addAttribute("profile", profileDto);
+        return "members/profile";
+    }
+
+    @GetMapping("/me/edit")
+    public String getMyProfileEdit(Model model) {
+        Long memberId = PrincipalUtils.getMemberId();
+        ProfileDto profile = memberService.readProfile(memberId);
+
+        model.addAttribute("profile", profile);
+        model.addAttribute("profileForm", UpdateProfileRequest.from(profile));
+        return "members/edit";
+    }
+
+    @PostMapping("/me/edit")
+    public String postMyProfileEdit(
+            @Valid @ModelAttribute("profileForm") UpdateProfileRequest request,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        Long memberId = PrincipalUtils.getMemberId();
+        if (bindingResult.hasErrors()) {
+            ProfileDto profile = memberService.readProfile(memberId);
+            model.addAttribute("profile", profile);
+            return "members/edit";
+        }
+        memberService.updateProfile(memberId, request);
+        return "redirect:/members/me";
+    }
+
+    @GetMapping("/{id}")
+    public String getIdProfile(
+            @PathVariable("id") Long memberId,
+            Model model
+    ){
+        ProfileDto profileDto = memberService.readProfile(memberId);
+        model.addAttribute("myProfile", PrincipalUtils.getMemberId().equals(memberId));
+        model.addAttribute("profile", profileDto);
+        return "members/profile";
+    }
+}
--- src/main/java/com/rejs/flashnote/domain/member/dto/request/UpdateProfileRequest.java
+++ src/main/java/com/rejs/flashnote/domain/member/dto/request/UpdateProfileRequest.java
@@ -1,16 +1,21 @@
 package com.rejs.flashnote.domain.member.dto.request;
 
+import com.rejs.flashnote.domain.member.dto.ProfileDto;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class UpdateProfileRequest {
     @NotEmpty
     private String name;
+
+    public static UpdateProfileRequest from(ProfileDto profile){
+        return UpdateProfileRequest.builder()
+                .name(profile.getName())
+                .build();
+    }
 }
