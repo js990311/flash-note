2da9265df28d1239a9b9f46b2636131edc433a58
Lee jong sik <102013392+js990311@users.noreply.github.com>
2026-02-16 23:02:25 +0900

    test: localStack 컨피그

--- src/test/java/com/rejs/flashnote/TestcontainersConfiguration.java
+++ src/test/java/com/rejs/flashnote/TestcontainersConfiguration.java
@@ -13,8 +13,13 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
@@ -37,4 +42,25 @@ public class TestcontainersConfiguration {
             registry.add("meilisearch.api-key", meilisearchContainer::getMasterKey);
         };
     }
+
+    @Bean
+    LocalStackContainer localStackContainer() {
+        return new LocalStackContainer(DockerImageName.parse("localstack/localstack:latest"))
+                .withServices(LocalStackContainer.Service.S3);
+    }
+
+    @Bean
+    @Primary
+    public S3Client s3Client(LocalStackContainer localStack) {
+        return S3Client.builder()
+                .endpointOverride(localStack.getEndpointOverride(LocalStackContainer.Service.S3))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(localStack.getAccessKey(), localStack.getSecretKey())
+                        )
+                )
+                .region(Region.of(localStack.getRegion()))
+                .build();
+    }
+
 }
